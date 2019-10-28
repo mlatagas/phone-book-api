abd01adb8af415e843927713161751957dc347a8
andile.mlata <andilem@bbd.co.za>
2019-10-28 18:31:57 +0200

    init

---
+++ src/main/java/com/phonebook/phonebookapi/PhonebookapiApplication.java
@@ -0,0 +1,13 @@
+package com.phonebook.phonebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonebookapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phonebook/phonebookapi/PhonebookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phonebook.phonebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
